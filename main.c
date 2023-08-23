7561d3aa93f270da0f2700b3bd86d8df3d414dde
Andreas Maucher <andreas-maucher@gmx.de>
2023-08-23 13:27:57 +0200

    added library, Makefile & wrote little main to test readline func

--- main.c
+++ main.c
@@ -1,11 +1,54 @@
-#include<unistd.h>
-#include<stdlib.h>
-#include<stdio.h>
-#include<stdarg.hn>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaucher <amaucher@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 10:13:39 by amaucher          #+#    #+#             */
+/*   Updated: 2023/08/23 10:13:42 by amaucher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "minishell.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 
-int main(void)
+/* LEXER: check for various tokens: 
+	1. redirections < >
+	2. $
+	3. '\'
+	4. whitespace
+	5. | 
+	6. check if it's a character string / word
+*/
+/* int *check_tokens(char *line)
 {
-//here comes stuff
-	return(1);
+	int i;
+	char *str;
+
+	i = 0;
+	while (line[i])
+	{
+		str = check_for_word(line);
+		i++;
+	}
+	return(str);
+} */
+
+int main(int ac, char **av)
+{
+	char *line;
+
+	(void) ac;
+	(void) av;
+	while(1)
+	{
+		line = readline("prompt: ");
+		add_history(line);
+		printf("%s", line);
+		free(line); // memory automatically allocated by readline function
+	}
 }
+
+//parse
---
+++ minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaucher <amaucher@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 10:16:54 by amaucher          #+#    #+#             */
+/*   Updated: 2023/08/23 10:16:56 by amaucher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
