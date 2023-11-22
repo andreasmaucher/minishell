33c2b8e792bcc7bcd1fffdb272847a5786a19935
Mihails Rizakovs <mrizakov@c4c2c4.42berlin.de>
2023-11-22 19:13:30 +0100

    Final Final

--- executor/heredocs.c
+++ executor/heredocs.c
@@ -6,7 +6,7 @@
 /*   By: mrizakov <mrizakov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 23:47:56 by mrizakov          #+#    #+#             */
-/*   Updated: 2023/11/22 04:06:52 by mrizakov         ###   ########.fr       */
+/*   Updated: 2023/11/22 18:54:21 by mrizakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,8 @@ void	ft_heredoc_loop(t_minishell *m, int fd, char *tmp_eof)
 	tmp_line = NULL;
 	ft_heredoc_exit_signal(m, fd);
 	line = readline("heredoc> ");
+	if (!line)
+		ft_heredoc_exit_eof(m, fd, tmp_line);
 	tmp_line = ft_strjoin(line, "\n");
 	free(line);
 	if (ft_strncmp(tmp_line, tmp_eof, ft_strlen(tmp_eof)) == 0 
@@ -61,6 +63,7 @@ void	ft_launch_heredoc(t_list *tmp, t_minishell *m)
 	if (pid == 0)
 	{
 		signal(SIGINT, handle_sigint_child);
+		signal(SIGQUIT, handle_sigint_child);
 		if_file_exists_delete(tmp->value);
 		fd = open(tmp->value, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
 		if (fd == -1) 
